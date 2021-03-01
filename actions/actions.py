c008c712f5302ab89879f73f3f69e83e4d2eac34
Petr Janik <peta.janik@email.cz>
2021-03-01 08:37:19 +0100

    Add custom form validation action

--- actions/actions.py
+++ actions/actions.py
@@ -1,27 +1,21 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from typing import Text, List, Optional
+from rasa_sdk.forms import FormValidationAction
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk import Tracker
 
 
-# This is a simple example for a custom action which utters "Hello World!"
+class ValidateNewsletterForm(FormValidationAction):
+    def name(self) -> Text:
+        return "validate_newsletter_form"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+    async def required_slots(
+            self,
+            slots_mapped_in_domain: List[Text],
+            dispatcher: "CollectingDispatcher",
+            tracker: "Tracker",
+            domain: "DomainDict",
+    ) -> Optional[List[Text]]:
+        if not tracker.get_slot("can_ask_age"):
+            slots_mapped_in_domain.remove("age")
+
+        return slots_mapped_in_domain
