6afcc58bf9149d0abb57c3472b3d53312d4de00e
Petr Janik <peta.janik@email.cz>
2021-03-01 18:40:37 +0100

    Complete submit action

--- actions/actions.py
+++ actions/actions.py
@@ -1,7 +1,54 @@
-from typing import Text, List, Optional
+from typing import Text, List, Optional, Dict, Any
 from rasa_sdk.forms import FormValidationAction
 from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk import Tracker
+from rasa_sdk import Tracker, Action
+from dotenv import load_dotenv
+import os
+import requests
+import json
+import uuid
+
+load_dotenv()
+
+airtable_api_key = os.getenv("AIRTABLE_API_KEY")
+base_id = os.getenv("BASE_ID")
+table_name = os.getenv("TABLE_NAME")
+
+
+def create_newsletter_record(email, frequency, notifications, can_ask_age, age):
+    request_url = f"https://api.airtable.com/v0/{base_id}/{table_name}"
+
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Authorization": f"Bearer {airtable_api_key}",
+    }
+
+    data = {
+        "fields": {
+            "Id": str(uuid.uuid4()),
+            "Email": email,
+            "Frequency": frequency,
+            "Notifications?": notifications,
+            "Can ask age?": can_ask_age,
+            "Age": age,
+        }
+    }
+
+    print(request_url)
+    print(headers)
+    print(json.dumps(data))
+
+    try:
+        response = requests.post(
+            request_url, headers=headers, data=json.dumps(data)
+        )
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        raise SystemExit(err)
+
+    print(f"Response status code: {response.status_code}")
+    return response
 
 
 class ValidateNewsletterForm(FormValidationAction):
@@ -19,3 +66,36 @@ class ValidateNewsletterForm(FormValidationAction):
             slots_mapped_in_domain.remove("age")
 
         return slots_mapped_in_domain
+
+
+class SubmitNewsletterForm(Action):
+
+    def name(self) -> Text:
+        return "submit_newsletter_form"
+
+    async def run(
+            self, dispatcher, tracker: Tracker, domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+        email = tracker.get_slot("email")
+        frequency = tracker.get_slot("frequency")
+        notifications = tracker.get_slot("notifications")
+        can_ask_age = tracker.get_slot("can_ask_age")
+        age = tracker.get_slot("age")
+
+        response = create_newsletter_record(email, frequency, notifications, can_ask_age, age)
+
+        dispatcher.utter_message("Thanks, your answers have been recorded!")
+
+        return []
+
+# class ActionSayStuff(Action):
+#     def name(self) -> Text:
+#         return "action_say_stuff"
+#
+#     def run(self,
+#             dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#         dispatcher.utter_message("Stuff")
+#
+#         return []
